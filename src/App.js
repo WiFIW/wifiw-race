a8e960cc808d082804e43515e5d5a623d2bd9583
Renato Galvão <renato@pulpowms.com>
2023-01-21 15:40:31 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { firebaseConfig } from './firebaseConfig';
+
+import { initializeApp } from "firebase/app";
+import { ref, get, getDatabase } from 'firebase/database';
+
 
 function App() {
+  const [ coaches, setCoaches ] = useState({});
+  const app = initializeApp(firebaseConfig);
+  const db = getDatabase(app);
+  const statsRef = ref(db, 'stats');
+  get(statsRef).then((snapshot) => {
+    if (snapshot.exists()) {
+      console.log(snapshot.val());
+      setCoaches(snapshot.val());
+    } else {
+      console.log("No data available");
+    }
+  }).catch((error) => {
+    console.error(error);
+  });
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {Object.keys(coaches).length > 0 ? Object.keys(coaches).map(coach => <li>{coach}</li>) : 'no coaches available'}
     </div>
   );
 }
---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAPP7yHbvuop8e4R23pLNX0tkokyZN5APQ",
+  authDomain: "wifiw-stats.firebaseapp.com",
+  databaseURL: "https://wifiw-stats.firebaseio.com",
+  projectId: "wifiw-stats",
+  storageBucket: "wifiw-stats.appspot.com",
+  messagingSenderId: "72719256534",
+  appId: "1:72719256534:web:a833879a0bd0e2ea767277"
+};
